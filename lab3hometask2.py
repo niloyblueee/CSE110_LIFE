3b584ab86af7c61d695cad82afe9441b8cf1419f
NIloy Blueee <143703109+niloyblueee@users.noreply.github.com>
2023-10-20 20:56:54 +0600

    lab3hometask2.py

--- lab3hometask2.py
+++ lab3hometask2.py
@@ -1,3 +1,13 @@
-n=int(input(''))
-res1=0
-for num in range(n):
+n=input('')
+n2=int(n)
+res=0
+for num in range(len(n),0,-1):
+    res2=n2//(10**(num-1))
+    n2=n2%(10**(num-1))    
+    if num==1:
+        print(res2,end='')
+    else:
+        print(res2,end=',')    
+print()
+
+#https://docs.google.com/document/d/1G5BGPpC3BWBuIO5YzaML-xssgctensS_/edit
