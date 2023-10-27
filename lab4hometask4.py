221c7fb0f8c219b9bc517515b7460704ae57619e
NIloy Blueee <143703109+niloyblueee@users.noreply.github.com>
2023-10-27 21:35:51 +0600

    Update lab4hometask4.py

--- lab4hometask4.py
+++ lab4hometask4.py
@@ -11,3 +11,19 @@ for index in range(len(s)):
     else:
         s2+=chr(ord(s[index]))     
 print(s2)        
+
+#Write a python program that takes a string as an input from 
+#the user and then modifies the string in such a way that the
+#string always starts with an uppercase letter and the case of
+#each subsequent letter is the opposite of the previous letter 
+#(uppercase character followed by a lowercase character followed
+#by an uppercase character and so on). Finally, the modified string
+#is printed to show the user
+#input:
+      #I       love         Python         Programming
+      #Python programming is very easy
+      #CSE110 Course
+#output:
+      #I       lOvE        pYtHoN        pRoGrAmMiNg
+      #PyThOn PrOgRaMmInG iS vErY eAsY
+      #CsE110 cOuRsE
