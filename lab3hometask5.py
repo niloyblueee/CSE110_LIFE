81f965359423acc3c5bdb1354aff2b2d05f20da6
NIloy Blueee <143703109+niloyblueee@users.noreply.github.com>
2023-10-29 01:04:40 +0600

    Update lab3hometask5.py

--- lab3hometask5.py
+++ lab3hometask5.py
@@ -3,3 +3,13 @@ for row in range(n+1):
     for column in range(1,row+1):
         print(column,end='')
     print()    
+#Write a python program that prints a right-angled 
+#triangle of height N using incrementing numbers where
+#N will be given as input.
+#Sample Input 1:
+#4
+#Sample Output 1:
+#1
+#12
+#123
+#1234
