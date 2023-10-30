e995ac415e457bbafe1fe9da0c1b9f4eed0e25d3
NIloy Blueee <143703109+niloyblueee@users.noreply.github.com>
2023-10-30 22:28:44 +0600

    Add files via upload

---
+++ PracticeOnString7.py
@@ -0,0 +1,14 @@
+n=int(input(''))
+for i in range(1,11):
+    print(n,'x',i,'=',n*i)
+    
+#Write a Python program that reads a 
+# number and displays the multiplication
+#  table of the given integer.
+#Sample Input: 15
+#Sample Output :
+#15X1=15
+#15X2=30
+#...
+#...
+#15X10=150  
