4880aa0a06690ab417d98b063089e7595ba81378
NIloy Blueee <143703109+niloyblueee@users.noreply.github.com>
2023-11-02 16:09:39 +0600

    lab4hometask2.py

--- lab4hometask2.py
+++ lab4hometask2.py
@@ -3,6 +3,8 @@ s2=''
 n1=0 #' '
 n2=0 #','
 n3=0
+s3=''
+s4=''
 for index in range(len(s1)):
     if s1[index]==',':
         n2=index
@@ -10,13 +12,24 @@ for index in range(len(s1)):
         n1=index
         n3+=n1+1
         break
-for index in range(n2):
-    s2+=s1[index]
-    for j in range(n3,len(s1)):
-        s2+=s1[j]
-        n3+=1
-        break
-print(s2,sep='')
+s3+=s1[0:n2]
+s4+=s1[n1+1:]        
+if len(s3)>len(s4):
+   for index in range(n2):
+      s2+=s1[index]
+      for j in range(n3,len(s1)):
+         s2+=s1[j]
+         n3+=1
+         break                 
+else:                               
+    for index in range(n2):
+      s2+=s1[index]
+      for j in range(n3,len(s1)):
+           s2+=s1[j]
+           n3+=1
+           break
+s2+=s1[n1+len(s3)+1:]            
+print(s2)
 #Write a Python program that will take one input from the user
 #made up of two strings separated by a comma and a space (see samples below).
 #Then create a mixed string with alternative characters from 
